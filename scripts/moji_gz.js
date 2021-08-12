0812c14d354f428d2ed0d1d3adbf2a22c005dbe1
ken5560 <42735847+ken5560@users.noreply.github.com>
2021-08-12 21:56:33 +0800

    Update moji_gz.js

--- scripts/moji_gz.js
+++ scripts/moji_gz.js
@@ -4,7 +4,7 @@
 */
 const $ = new Env('墨迹天气');
 const notify = $.isNode() ? require('./sendNotify') : '';
-const City = encodeURIComponent($.getdata('city')||"广州市")  //可在此处修改城市
+const City = encodeURIComponent($.getdata('city')||"2")  //可在此处修改城市
 const j = $.getdata('citynum')||"5";
 let reduction = $.getdata('cut') || 'false'; //日志
 let daylys = $.getdata('day_desc') || 'true', //每日天气
